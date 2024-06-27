87e354a86a93226f9ef11f9b24d62d37fc3a22dd
Hadi14 <asusn43@yahoo.com>
2024-06-27 07:30:20 +0330

    correct no logging in server

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
-app.use(express.static('public'));// کاربر با اتصال به سایت میتواند به فایلهای این پوشه دسترسی داشته باشه
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');// صفحه پیش فرض سایت
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 let connectedPeers = [];
 let connectedPeersStrangers = [];
 
-io.on("conncection", (socket) => {
+io.on('connection', (socket) => {
+    console.log("AAAAAAAAAAAAAAAAAA");
     console.log("SERVER:user connected to socket IO ..." + socket.id);
     connectedPeers.push(socket.id);
     console.log(connectedPeers);
